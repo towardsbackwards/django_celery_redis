3454d3ae6df1dec085df3367386984ae4fd4c1c5
Pavel Martynov <python_dev@omnicdek.ru>
2023-11-27 12:52:24 +0300

    03 folded serializer

--- service/services/views.py
+++ service/services/views.py
@@ -7,7 +7,9 @@ from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all().prefetch_related(
-        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')))
+    queryset = Subscription.objects.all().prefetch_related('plan',
+                                                           Prefetch('client',
+                                                                    queryset=Client.objects.all().select_related(
+                                                                        'user').only('company_name', 'user__email')))
 
     serializer_class = SubscriptionSerializer
