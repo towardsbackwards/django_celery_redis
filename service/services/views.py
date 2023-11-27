4abfaf14dcd9519a15c6f4ce945bcc219e3fd95f
Pavel Martynov <python_dev@omnicdek.ru>
2023-11-27 13:39:58 +0300

    04 2 total amount aggregation via sql query

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -10,6 +10,15 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related('plan',
                                                            Prefetch('client',
                                                                     queryset=Client.objects.all().select_related(
-                                                                        'user').only('company_name', 'user__email')))
+                                                                        'user').only('company_name',
+                                                                                     'user__email'))).annotate(
+        price=F('service__full_price') * (1 - (F('plan__discount_percent') / 100.00)))
 
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):  # will change in future
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+        response_data = {'result': response.data, 'total_amount': queryset.aggregate(total=Sum('price')).get('total')}
+        response.data = response_data
+        return response
